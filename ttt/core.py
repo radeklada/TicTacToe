fc49841b10b454ebcd75faa051112844971f5d08
radeklada <radoslaw.lada@outlook.com>
2021-11-15 21:53:49 +0100

    migration update, inviting, creating game

--- ttt/core.py
+++ ttt/core.py
@@ -1,4 +1,40 @@
-CROSS_SYMBOL = "x"
-CIRCLE_SYMBOL = "o"
-MAX_POSITION = 8
-MIN_POSITION = 0
+import random
+import string
+
+from django.utils import timezone
+
+from ttt.models import GameSession, Game
+
+
+def rand_external_id():
+    selected_letters = random.sample(string.ascii_letters, GameSession.EXTERNAL_ID_SIZE)
+    return ''.join(selected_letters)
+
+
+def invite(host_user, guest_user):
+    player_1, player_2 = sorted([host_user, guest_user], key=lambda user: user.id)
+    return GameSession.objects.create(external_id=rand_external_id(), player_1=player_1, player_2=player_2)
+
+
+class GameError(Exception):
+    pass
+
+
+def create_game(game_session):
+    game = Game.objects.filter(session=game_session).order_by('-created_at').first()
+    if game is not None:
+        if not game.result:
+            raise GameError()
+        else:
+            cross_player = game.circle_player
+            circle_player = game.cross_player
+    else:
+        cross_player = game_session.player_1
+        circle_player = game_session.player_2
+    return Game.objects.create(
+        session=game_session,
+        cross_player=cross_player,
+        circle_player=circle_player,
+        created_at=timezone.now(),
+        result=None
+    )
--- ttt/models.py
+++ ttt/models.py
@@ -1,28 +1,46 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
-from ttt.validators import validate_symbol, validate_position
+
+def validate_symbol(value):
+    if value != Move.CROSS_SYMBOL and value != Move.CIRCLE_SYMBOL:
+        raise ValidationError(f"invalid symbol: {value}")
 
 
-class Invitation(models.Model):
-    external_id = models.CharField(max_length=12, unique=True)
-    host = models.ForeignKey(User, on_delete=models.PROTECT)
+def validate_position(value):
+    if value < Move.MIN_POSITION or Move.MAX_POSITION < value:
+        raise ValidationError(f"invalid position: {value}")
+
+def validate_result(value):
+    if value != Move.CROSS_SYMBOL and value != Move.CIRCLE_SYMBOL and value != Game.DRAW:
+        raise ValidationError(f"Invalid result: {value}")
 
 
 class GameSession(models.Model):
-    external_id = models.CharField(max_length=12, unique=True)
+    EXTERNAL_ID_SIZE = 12
+    external_id = models.CharField(max_length=EXTERNAL_ID_SIZE, unique=True)
     player_1 = models.ForeignKey(User, on_delete=models.PROTECT, related_name="player_1")
     player_2 = models.ForeignKey(User, on_delete=models.PROTECT, related_name="player_2")
 
+    class Meta:
+        unique_together = ["player_1", "player_2"]
+
 
 class Game(models.Model):
+    DRAW = "d"
     session = models.ForeignKey(GameSession, on_delete=models.PROTECT)
     cross_player = models.ForeignKey(User, on_delete=models.PROTECT, related_name="cross_player")
     circle_player = models.ForeignKey(User, on_delete=models.PROTECT, related_name="circle_player")
     created_at = models.DateTimeField()
+    result = models.CharField(max_length=1, validators=[validate_result], null=True)
 
 
 class Move(models.Model):
+    CROSS_SYMBOL = "x"
+    CIRCLE_SYMBOL = "o"
+    MAX_POSITION = 8
+    MIN_POSITION = 0
     game = models.ForeignKey(Game, on_delete=models.PROTECT)
     position = models.IntegerField(validators=[validate_position])
     symbol = models.CharField(max_length=1, validators=[validate_symbol])
